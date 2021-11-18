31574f476e0138c234953a108e95f3d7f5d9ac24
dtnngts <dotaningtias100219@students.unila.ac.id>
2021-11-18 13:47:34 +0700

    db dota

--- app/Database/Migrations/2021-11-18-061247_Migrations.php
+++ app/Database/Migrations/2021-11-18-061247_Migrations.php
@@ -25,7 +25,7 @@ class Crud extends Migration
                         ],
                         'nominal_uang' => [
                         'type' => 'VARCHAR',
-                        'constraint' => 150,
+                        'constraint' => 100,
                         'null' => true
                         ]
                     ]);
@@ -33,8 +33,6 @@ class Crud extends Migration
             $this->forge->addKey('id', true);
 			$this->forge->createTable('money');
     }
-
-
 	public function down()
 	{
 			$this->forge->dropTable('money');
