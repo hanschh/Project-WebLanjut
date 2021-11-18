45cc96adf403f82ad844f1359659bea599215f70
dtnngts <dotaningtias100219@students.unila.ac.id>
2021-11-18 13:29:21 +0700

    db dota

---
+++ app/Database/Migrations/2021-11-18-061247_Migrations.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Crud extends Migration
+{
+    public function up()
+    {
+                    $this->forge->addField([
+                        'id' => [
+                        'type' => 'INT',
+                        'constraint' => 11,
+                        'unsigned' => true,
+                        'auto_increment' => true
+                        ],
+                        'nama' => [
+                        'type' => 'VARCHAR',
+                        'constraint' => 100,
+                        ],
+                        'no_rekening' => [
+                        'type' => 'VARCHAR',
+                        'constraint' => 11,
+                        ],
+                        'nominal_uang' => [
+                        'type' => 'VARCHAR',
+                        'constraint' => 150,
+                        'null' => true
+                        ]
+                    ]);
+
+            $this->forge->addKey('id', true);
+			$this->forge->createTable('money');
+    }
+
+
+	public function down()
+	{
+			$this->forge->dropTable('money');
+	}
+}
